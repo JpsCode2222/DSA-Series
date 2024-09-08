fc2fbadb17a9d0f198ecb2339cb2bb32aa0d41c9
jayad <jayad.pathan@techprimelab.com>
2024-09-08 18:17:38 +0530

    learn function in cpp

--- 4_functions.cpp
+++ 4_functions.cpp
@@ -27,11 +27,73 @@ using namespace std;
         }
     }
 
+    double calculateSum(int n){
+        int sum = 0;
+        for(int i=1;i<=n;i++){
+            sum += i;
+        }
+        cout<<"Sum is : "<<sum;
+        return sum;
+    }
+
+    double factorial(int n){
+        int fact = 1;
+        for(int i=1;i<=n;i++){
+            fact *= i;
+        }
+        cout<<"Factorail of "<<n<<" : "<<fact;
+        return fact;
+    }
+
+    int calculateSumOfDigits(int num){
+        int sum = 0;
+        while(num>0){
+            sum += num % 10;
+            num = num / 10;
+        }
+        cout<<" sum is "<<sum;
+    }
+
+    checkPrime(int n){
+        for(int i=2;i<n;i++){
+            if(n%i == 0){
+                // cout<<"Number is not prime : "<<n<<endl;
+                return n;
+            }
+        }
+        cout<<"Number is Prime : "<<n<<endl;
+        return n;
+    }
+
+    void primeNumbers(int n){
+        for(int i=1;i<=n;i++){
+            checkPrime(i);
+        }
+    }
+
+    void fibonacci(int n){
+        int a = 0;
+        int b = 1;
+        int fib = a+b;
+        cout<<a<<endl<<b<<endl;
+        if(n<2){
+            return;
+        }
+        for(int i=1;i<=n-2;i++){
+            cout<<fib<<endl;
+            fib+=i;
+        }
+    }
+
 int main(){
 
     // printHello();
     // printAge(20);
-    calculateMin(10,30);
-
+    // calculateMin(10,30);
+    // calculateSum(100);
+    // factorial(5);
+    // calculateSumOfDigits(123);
+    // primeNumbers(60);
+    fibonacci(4);
     return 0;
 }
